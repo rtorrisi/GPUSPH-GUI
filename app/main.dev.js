01561ce4ba306c9c32f186506bccaa3af1c795a1
Riccardo Torrisi <r.torrisi96@gmail.com>
2018-12-23 13:37:13 +0100

    Disable security warnings

--- app/main.dev.js
+++ app/main.dev.js
@@ -15,6 +15,8 @@ import { autoUpdater } from 'electron-updater';
 import log from 'electron-log';
 import MenuBuilder from './menu';
 
+process.env.ELECTRON_DISABLE_SECURITY_WARNINGS = true;
+
 export default class AppUpdater {
   constructor() {
     log.transports.file.level = 'info';
