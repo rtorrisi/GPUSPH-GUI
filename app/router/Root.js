1ba1abda72bec4ec82b486ccb3ff3f103188952d
Riccardo Torrisi <r.torrisi96@gmail.com>
2018-12-28 12:14:17 +0100

    Remove unused route

--- app/router/Root.js
+++ app/router/Root.js
@@ -1,6 +1,5 @@
 // @flow
-import * as React from 'react';
-
+import React, { Component, Fragment } from 'react';
 import { Provider } from 'react-redux';
 import { Switch, Route } from 'react-router';
 import { ConnectedRouter } from 'connected-react-router';
@@ -8,8 +7,6 @@ import { ConnectedRouter } from 'connected-react-router';
 import type { Store } from '../reducers/types';
 
 import routes from './routes';
-
-import CounterPage from '../containers/CounterPage';
 import HomePage from '../containers/HomePage';
 
 type Props = {
@@ -17,18 +14,17 @@ type Props = {
   history: {}
 };
 
-export default class Root extends React.Component<Props> {
+export default class Root extends Component<Props> {
   render() {
     const { store, history } = this.props;
     return (
       <Provider store={store}>
         <ConnectedRouter history={history}>
-          <React.Fragment>
+          <Fragment>
             <Switch>
-              <Route path={routes.COUNTER} component={CounterPage} />
               <Route path={routes.HOME} component={HomePage} />
             </Switch>
-          </React.Fragment>
+          </Fragment>
         </ConnectedRouter>
       </Provider>
     );
