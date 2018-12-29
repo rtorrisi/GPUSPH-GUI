72c4e711515d42ec112d1f6b0b10c0153bc9a28f
Riccardo Torrisi <r.torrisi96@gmail.com>
2018-12-29 07:43:45 +0100

    Create HomePage container

--- app/containers/HomePage.js
+++ app/containers/HomePage.js
@@ -1,13 +1,27 @@
-// @flow
-import React, { Component } from 'react';
-import Home from '../components/Home';
+import { bindActionCreators } from 'redux';
+import { connect } from 'react-redux';
+import Dashboard from '../components/Dashboard';
+import * as instanceActions from '../actions/instanceActions';
 
-type Props = {};
-
-export default class HomePage extends Component<Props> {
-  props: Props;
-
-  render() {
-    return <Home />;
-  }
+function mapStateToProps(state) {
+  return {
+    version: state.instance.version,
+    problemName: state.instance.problemName,
+    HDF5: state.instance.HDF5,
+    MPI: state.instance.MPI,
+    chrono: state.instance.chrono,
+    capability: state.instance.capability,
+    maxIter: state.instance.maxIter,
+    outDir: state.instance.outDir,
+    simulation: state.instance.simulation
+  };
 }
+
+function mapDispatchToProps(dispatch) {
+  return bindActionCreators(instanceActions, dispatch);
+}
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(Dashboard);
