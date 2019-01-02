941797cc472774c17b06922617c4cb9ca29f73e6
Riccardo Torrisi <r.torrisi96@gmail.com>
2019-01-02 11:06:17 +0100

    Create SettingsLayout layout

---
+++ app/layouts/SettingsLayout.jsx
@@ -0,0 +1,31 @@
+import React, { Component } from 'react';
+import { withStyles } from '@material-ui/core/styles';
+
+import Paper from '@material-ui/core/Paper';
+
+type Props = {
+  classes: {}
+};
+
+class SimulateLayout extends Component<Props> {
+  props: Props;
+
+  render() {
+    const { classes } = this.props;
+    const { paperStyle } = classes;
+
+    return <Paper className={paperStyle}>Settings Here</Paper>;
+  }
+}
+
+const wstyles = () => ({
+  paperStyle: {
+    flex: 1,
+    backgroundColor: '#313641',
+    margin: '10px',
+    minHeight: '100px',
+    overflowX: 'auto'
+  }
+});
+
+export default withStyles(wstyles, { withTheme: true })(SimulateLayout);
